98e92853aa4b48c257fdefb26c7929f20faeac27
cinku <cinq2ez@gmail.com>
2019-02-28 20:22:54 +0100

    Add flask simple app with cors enabled

---
+++ api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, Response
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+@app.route('/items')
+def api_articles():
+    items = ['item1', 'item2', 'item3', 'item4']
+    return Response(json.dumps(items),  mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run()
